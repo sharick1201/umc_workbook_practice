5beead47fac23a4003477c988302ca8ec5562f6c
Sieun Park <park12010@naver.com>
2024-11-10 12:14:15 +0900

    init

---
+++ umc_workbook_practice/src/main/java/com/example/umc_workbook_practice/UmcWorkbookPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.umc_workbook_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcWorkbookPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcWorkbookPracticeApplication.class, args);
+	}
+
+}
