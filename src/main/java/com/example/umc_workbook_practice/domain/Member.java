68080da2e17a4eca4360c982268ad12080981f14
Sharick1201 <park12010@naver.com>
2024-11-10 20:37:05 +0900

    init

---
+++ src/main/java/com/example/umc_workbook_practice/domain/Member.java
@@ -0,0 +1,19 @@
+package com.example.umc_workbook_practice.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ src/test/java/com/example/umc_workbook_practice/UmcWorkbookPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc_workbook_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcWorkbookPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
