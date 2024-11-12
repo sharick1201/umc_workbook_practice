1b84a7225d6eec4c623e0ef79dda766abc4487a2
sharick <park12010@naver.com>
2024-11-12 17:53:02 +0900

    BaseEntity 생성

--- src/main/java/com/example/umc_workbook_practice/UmcWorkbookPracticeApplication.java
+++ src/main/java/com/example/umc_workbook_practice/UmcWorkbookPracticeApplication.java
@@ -2,8 +2,10 @@ package com.example.umc_workbook_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UmcWorkbookPracticeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/umc_workbook_practice/domain/Member.java
+++ src/main/java/com/example/umc_workbook_practice/domain/Member.java
@@ -1,5 +1,6 @@
 package com.example.umc_workbook_practice.domain;
 
+import com.example.umc_workbook_practice.domain.common.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,7 +12,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
